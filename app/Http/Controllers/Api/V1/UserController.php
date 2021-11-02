31e2350b058485343366e08656a41221e2cf43d1
gy <342720006@qq.com>
2021-11-02 23:46:27 +0800

    openid

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -8,12 +8,16 @@ use Illuminate\Http\Request;
 use App\Models\WxQrCode as wqcm;
 use Illuminate\Support\Facades\DB;
 use App\Models\Order ;
+use App\Services\WechatServices as ws;
 class UserController extends Controller
 {
 
     // 获取小程序openid
     public function getWeiOpenId(Request $request){
 
+        $data = (new ws)->index();
+
+        return r::rMsgData(200,'ok',$data);       
     }
 
 }
